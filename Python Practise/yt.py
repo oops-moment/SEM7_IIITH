5557801f1691d62a1d309489f2b3db0e1ee3e3fa
oops-moment <prisha.kumar@students.iiit.ac.in>
2024-10-09 19:52:50 +0530

    practise

---
+++ Python Practise/yt.py
@@ -0,0 +1,41 @@
+# # Youtube , instagram, spotify all programmed in python
+
+# print("Hello World")
+
+# #  declaring variables
+# age = 20
+# price = 19.95
+# print(age)
+# print(price)
+
+# first_name = "Prisha"
+# last_name = "Arora"
+
+# bool_value= True
+
+# first_name ="John"
+# last_name = "Smith"
+
+# age= 20
+
+# new_patient = True
+
+# # name = input("What is the patients name ? ")
+# # print("name" + name)
+
+# #  three types of data : number , string , boolean
+# birth_year = input("Enter your birth year: ")  #-->always a string 
+# print(type(birth_year))
+
+# age= 2024-int(birth_year)
+# print(age)
+
+first_number=input("Enter the first number")
+second_number= input("Enter the second number")
+
+first_number=float(first_number)
+second_number=float(second_number)
+
+print("additive value "+ str(first_number+second_number))
+
+
