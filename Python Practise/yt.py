1a35746a2b0710c73e7929453c5b3db4517c60e5
oops-moment <prisha.kumar@students.iiit.ac.in>
2024-10-09 23:04:45 +0530

    Refactor Python Practise/yt.py

--- Python Practise/yt.py
+++ Python Practise/yt.py
@@ -1,41 +1,57 @@
-# # Youtube , instagram, spotify all programmed in python
+# # # Youtube , instagram, spotify all programmed in python
 
-# print("Hello World")
+# # print("Hello World")
 
-# #  declaring variables
-# age = 20
-# price = 19.95
-# print(age)
-# print(price)
+# # #  declaring variables
+# # age = 20
+# # price = 19.95
+# # print(age)
+# # print(price)
 
-# first_name = "Prisha"
-# last_name = "Arora"
+# # first_name = "Prisha"
+# # last_name = "Arora"
 
-# bool_value= True
+# # bool_value= True
 
-# first_name ="John"
-# last_name = "Smith"
+# # first_name ="John"
+# # last_name = "Smith"
 
-# age= 20
+# # age= 20
 
-# new_patient = True
+# # new_patient = True
 
-# # name = input("What is the patients name ? ")
-# # print("name" + name)
+# # # name = input("What is the patients name ? ")
+# # # print("name" + name)
 
-# #  three types of data : number , string , boolean
-# birth_year = input("Enter your birth year: ")  #-->always a string 
-# print(type(birth_year))
+# # #  three types of data : number , string , boolean
+# # birth_year = input("Enter your birth year: ")  #-->always a string 
+# # print(type(birth_year))
 
-# age= 2024-int(birth_year)
-# print(age)
+# # age= 2024-int(birth_year)
+# # print(age)
 
-first_number=input("Enter the first number")
-second_number= input("Enter the second number")
+# first_number=input("Enter the first number")
+# second_number= input("Enter the second number")
 
-first_number=float(first_number)
-second_number=float(second_number)
+# first_number=float(first_number)
+# second_number=float(second_number)
 
-print("additive value "+ str(first_number+second_number))
+# print("additive value "+ str(first_number+second_number))
 
+# #  strings 
 
+course = "Python for beginners"
+#  methods to specific to string
+
+print(course.upper())
+print(course)
+
+print(course.find('t'))
+
+print(course.replace('for','4'))
+
+#  note that in all this original string remained unchanged that is that remains immutable irrespective
+
+print(course.replace('g','0'))
+
+print('Python' in course)
