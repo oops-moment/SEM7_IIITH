cf181583e490be05a48b1a89c542de8ccf4fafb8
oops-moment <prisha.kumar@students.iiit.ac.in>
2024-10-09 23:56:14 +0530

    Refactor Python Practise/yt.py: Clean up code and remove commented out sections

--- Python Practise/yt.py
+++ Python Practise/yt.py
@@ -1,57 +1,105 @@
-# # # Youtube , instagram, spotify all programmed in python
+# # # # Youtube , instagram, spotify all programmed in python
 
-# # print("Hello World")
+# # # print("Hello World")
 
-# # #  declaring variables
-# # age = 20
-# # price = 19.95
-# # print(age)
-# # print(price)
+# # # #  declaring variables
+# # # age = 20
+# # # price = 19.95
+# # # print(age)
+# # # print(price)
 
-# # first_name = "Prisha"
-# # last_name = "Arora"
+# # # first_name = "Prisha"
+# # # last_name = "Arora"
 
-# # bool_value= True
+# # # bool_value= True
 
-# # first_name ="John"
-# # last_name = "Smith"
+# # # first_name ="John"
+# # # last_name = "Smith"
 
-# # age= 20
+# # # age= 20
 
-# # new_patient = True
+# # # new_patient = True
 
-# # # name = input("What is the patients name ? ")
-# # # print("name" + name)
+# # # # name = input("What is the patients name ? ")
+# # # # print("name" + name)
 
-# # #  three types of data : number , string , boolean
-# # birth_year = input("Enter your birth year: ")  #-->always a string 
-# # print(type(birth_year))
+# # # #  three types of data : number , string , boolean
+# # # birth_year = input("Enter your birth year: ")  #-->always a string 
+# # # print(type(birth_year))
 
-# # age= 2024-int(birth_year)
-# # print(age)
+# # # age= 2024-int(birth_year)
+# # # print(age)
 
-# first_number=input("Enter the first number")
-# second_number= input("Enter the second number")
+# # first_number=input("Enter the first number")
+# # second_number= input("Enter the second number")
 
-# first_number=float(first_number)
-# second_number=float(second_number)
+# # first_number=float(first_number)
+# # second_number=float(second_number)
 
-# print("additive value "+ str(first_number+second_number))
+# # print("additive value "+ str(first_number+second_number))
 
-# #  strings 
+# # #  strings 
 
-course = "Python for beginners"
-#  methods to specific to string
+# course = "Python for beginners"
+# #  methods to specific to string
 
-print(course.upper())
-print(course)
+# print(course.upper())
+# print(course)
 
-print(course.find('t'))
+# print(course.find('t'))
 
-print(course.replace('for','4'))
+# print(course.replace('for','4'))
 
-#  note that in all this original string remained unchanged that is that remains immutable irrespective
+# #  note that in all this original string remained unchanged that is that remains immutable irrespective
 
-print(course.replace('g','0'))
+# print(course.replace('g','0'))
 
-print('Python' in course)
+# print('Python' in course)
+
+# print(10/3)
+
+# print(10//3)
+
+
+# X = 10
+# X = X + 3
+# X += 3
+# X -= 3
+
+# print(X)
+
+
+# #  Operator Precedence
+
+# X = 10 + 3*2
+# print(X)
+
+# X=(10+3)*2
+# print(X)
+
+# X= 3 >2
+# print(X)
+
+# X= 3==2
+# print(X)
+
+# #  operators are > >= < <= == !=
+
+# # logical operators 
+
+# X=30
+# print(not (X >10 and X<40))
+
+# #  and when both expression are true, or whren atleast one expression is truye
+
+# if (True):
+#     print("yo")
+#     print("yo2")
+
+
+# # concept of while lop
+
+# i = 1
+# while i<=5 : 
+#     print(i * '*')
+#     i+=1
